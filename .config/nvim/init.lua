7d8465fc80021ba4bb2d39ab849e76169d4e85f6
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-04-19 10:39:12 +0200

    set options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true --MARLENE set true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true --MARLENE uncomment
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
@@ -956,7 +956,7 @@ require("lazy").setup({
 		-- See `:help lualine.txt`
 		opts = {
 			options = {
-				icons_enabled = false,
+				icons_enabled = vim.g.have_nerd_font,
 				theme = "auto",
 				component_separators = "|",
 				section_separators = "",
