9579105da88243f9b81ef53781021266974f6d2a
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-05-04 09:40:31 +0200

    added latest kickstart fix 6f6f38a

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -581,6 +581,14 @@ require("lazy").setup({
 							group = highlight_augroup,
 							callback = vim.lsp.buf.clear_references,
 						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
 					end
 
 					-- The following autocommand is used to enable inlay hints in your
@@ -595,13 +603,6 @@ require("lazy").setup({
 				end,
 			})
 
-			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-				callback = function(event)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event.buf })
-				end,
-			})
 			-- LSP servers and clients are able to communicate to each other what features they support.
 			--  By default, Neovim doesn't support everything that is in the LSP specification.
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
