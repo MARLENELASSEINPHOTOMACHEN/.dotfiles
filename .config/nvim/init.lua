914c41b7c35138564ad424e440b30861a3681934
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-06-05 22:36:26 +0200

    added noice.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -926,7 +926,7 @@ require("lazy").setup(
 			"nvim-treesitter/nvim-treesitter",
 			build = ":TSUpdate",
 			opts = {
-				ensure_installed = { --MARLENE added css, ts, js
+				ensure_installed = { --MARLENE added css, ts, js, regex
 					"bash",
 					"c",
 					"diff",
@@ -940,6 +940,7 @@ require("lazy").setup(
 					"markdown",
 					"vim",
 					"vimdoc",
+					"regex",
 				},
 				-- Autoinstall languages that are not installed
 				auto_install = true,
@@ -1094,6 +1095,38 @@ require("lazy").setup(
 			end,
 			-- Optional dependencies
 			dependencies = { "nvim-tree/nvim-web-devicons" },
+		},
+		-- lazy.nvim
+		{
+			"folke/noice.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- add any options here
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			},
+			dependencies = {
+				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+				"MunifTanjim/nui.nvim",
+				-- OPTIONAL:
+				--   `nvim-notify` is only needed, if you want to use the notification view.
+				--   If not available, we use `mini` as the fallback
+				"rcarriga/nvim-notify",
+			},
 		},
 	}, -- NOTE: end of custom plugins
 	{
