9bb5d92152778a5beedbaf24e7aa37c31e435d71
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-06-12 10:19:47 +0200

    added mouse scroll fix for animate

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -918,8 +918,30 @@ require("lazy").setup(
 
 				--  MARLENE: more
 				-- Animate vim motions
+				local mouse_scrolled = false
+				for _, scroll in ipairs({ "Up", "Down" }) do
+					local key = "<ScrollWheel" .. scroll .. ">"
+					vim.keymap.set("", key, function()
+						mouse_scrolled = true
+						return key
+					end, { noremap = true, expr = true })
+				end
 
-				require("mini.animate").setup()
+				local animate = require("mini.animate")
+				animate.setup({
+					scroll = {
+						timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+						subscroll = animate.gen_subscroll.equal({
+							predicate = function(total_scroll)
+								if mouse_scrolled then
+									mouse_scrolled = false
+									return false
+								end
+								return total_scroll > 1
+							end,
+						}),
+					},
+				})
 			end,
 		},
 		{ -- Highlight, edit, and navigate code
