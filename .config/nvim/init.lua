fe5b2c7b6dd7e8a80f0ec43b80ce568de08e5bb0
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-06-24 12:48:50 +0200

    removed precognition

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1069,8 +1069,10 @@ require("lazy").setup(
 						icons_enabled = vim.g.have_nerd_font,
 						-- theme = "auto",
 						theme = seoul256_light,
-						component_separators = "|",
-						section_separators = "",
+						-- component_separators = "|",
+						-- section_separators = "",
+						component_separators = "",
+						section_separators = { left = "", right = "" },
 					},
 				})
 			end,
@@ -1152,22 +1154,38 @@ require("lazy").setup(
 				end
 			end,
 		},
-
 		-- {
-		-- 	"tris203/precognition.nvim",
+		-- 	"folke/noice.nvim",
 		-- 	event = "VeryLazy",
-		-- 	opts = {},
-		-- 	keys = {
-		-- 		{
-		-- 			"<leader>tp", --MARLENE changed to capital F to avoid conflict with Harpoon keys
-		-- 			function()
-		-- 				require("precognition").toggle()
-		-- 			end,
-		-- 			mode = "",
-		-- 			desc = "[T]oggle [p]recognition",
+		-- 	opts = {
+		-- 		-- add any options here
+		-- 		lsp = {
+		-- 			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		-- 			override = {
+		-- 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+		-- 				["vim.lsp.util.stylize_markdown"] = true,
+		-- 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		-- 			},
+		-- 		},
+		-- 		-- you can enable a preset for easier configuration
+		-- 		presets = {
+		-- 			bottom_search = true, -- use a classic bottom cmdline for search
+		-- 			command_palette = true, -- position the cmdline and popupmenu together
+		-- 			long_message_to_split = true, -- long messages will be sent to a split
+		-- 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		-- 			lsp_doc_border = false, -- add a border to hover docs and signature help
 		-- 		},
 		-- 	},
+		-- 	dependencies = {
+		-- 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		-- 		"MunifTanjim/nui.nvim",
+		-- 		-- OPTIONAL:
+		-- 		--   `nvim-notify` is only needed, if you want to use the notification view.
+		-- 		--   If not available, we use `mini` as the fallback
+		-- 		"rcarriga/nvim-notify",
+		-- 	},
 		-- },
+		--
 	}, -- NOTE: end of custom plugins
 	{
 		ui = {
