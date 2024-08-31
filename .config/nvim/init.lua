dbac9d544b220dbde178e1ef5d66bf8c80f408e0
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-08-31 16:38:40 +0200

    added back keymap documentation for harpoon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -376,6 +376,7 @@ require("lazy").setup(
 					{ "<leader>s", group = "[S]earch" },
 					{ "<leader>w", group = "[W]orkspace" },
 					{ "<leader>t", group = "[T]oggle" },
+					{ "<leader>f", group = "Harpoon [f]ishing" }, --MARLENE harpoon keymaps
 					{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 				},
 			},
