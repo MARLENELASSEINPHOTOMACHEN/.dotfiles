f8c67ab2b7c6d6c46088cd741258d9332f1b320d
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-09-23 23:34:04 +0200

    renamed tsserver kickstart 7201dc4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -691,8 +691,8 @@ require("lazy").setup(
 					-- Some languages (like typescript) have entire language plugins that can be useful:
 					--    https://github.com/pmizio/typescript-tools.nvim
 					--
-					-- But for many setups, the LSP (`tsserver`) will work just fine
-					tsserver = {},
+					-- But for many setups, the LSP (`ts_ls`) will work just fine
+					ts_ls = {},
 					--
 
 					lua_ls = {
