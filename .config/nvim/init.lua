0b0d005cc9b8e5c041c7d5724811425ee5de423d
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2024-10-07 21:29:51 +0200

    commented uncommon lsps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -676,7 +676,7 @@ require("lazy").setup(
 				--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 				local servers = {
 					-- clangd = {},
-					gopls = {},
+					-- gopls = {},
 					-- pyright = {},
 					-- rust_analyzer = {},
 					-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -684,7 +684,7 @@ require("lazy").setup(
 					cssls = {},
 					-- biome = {}, --MARLENE: not doing anything atm
 					svelte = {}, --MARLENE: ensure :TSInstall svelte typescript javascript css html jsdoc --are installed
-					elixirls = {},
+					-- elixirls = {}, --MARLENE
 
 					prettierd = {}, --MARLENE: replacement for biome for the time being
 					--
