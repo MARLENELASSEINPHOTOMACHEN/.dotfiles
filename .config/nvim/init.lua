cfcf12a92e738c645258b090145c8536c4fc2aa0
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2025-10-28 13:56:12 +0100

    kickstart options set

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true --MARLENE to true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true --MARLENE uncommented
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = "a"
