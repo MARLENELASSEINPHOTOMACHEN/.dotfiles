c2b7c7a62430ab04627e2e0c33e02d49833d7373
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2025-12-15 22:57:54 +0100

    added treesitter context

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1306,6 +1306,28 @@ require("lazy").setup({
 		---@type render.md.UserConfig
 		opts = {},
 	},
+	{ --add Sticky Context line to the top
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+				line_numbers = true,
+				multiline_threshold = 20, -- Maximum number of lines to show for a single context
+				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				-- Separator between context and content. Should be a single character string, like '-'.
+				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+				separator = nil,
+				zindex = 20, -- The Z-index of the context window
+				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+			})
+			vim.keymap.set("n", "<leader>tc", function()
+				require("treesitter-context").toggle()
+			end, { desc = "[t]oggle [c]ontext" })
+		end,
+	},
 	-- add new plugins right above this line
 }, {
 	ui = {
