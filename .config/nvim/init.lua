21299d5b6b0917dbb79b816b55d089f98ba6a9c5
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-02-20 03:12:06 +0100

    added harpoon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1166,6 +1166,33 @@ require("lazy").setup({
 		---@type ibl.config
 		opts = {},
 	},
+	{ -- Quick Access to file
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup({
+				settings = {
+					save_on_toggle = true,
+					sync_on_ui_close = true,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>H", function()
+				harpoon:list():add()
+			end, { desc = "[H]arpoon file" })
+			vim.keymap.set("n", "<leader>h", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = "[H]arpoon Quick Menu" })
+			-- Set <space>1..<space>5 be my shortcuts to moving to the files
+			for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+				vim.keymap.set("n", string.format("<leader>%d", idx), function()
+					harpoon:list():select(idx)
+				end, { desc = string.format("Harpoon file no. %d", idx) })
+			end
+		end,
+	},
 	-- MARLENE add new plugins right above this line
 }, {
 	ui = {
