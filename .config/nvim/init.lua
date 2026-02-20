bd498fa265c351362c4cf4d5853c56f1b80f8ce9
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-02-20 03:12:36 +0100

    added render markdown

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1208,6 +1208,15 @@ require("lazy").setup({
 			vim.diagnostic.config({ virtual_text = false })
 		end,
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 	-- MARLENE add new plugins right above this line
 }, {
 	ui = {
