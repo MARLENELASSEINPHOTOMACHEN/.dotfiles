a2583bd1ab24c76cc3fa381b56d47f09d5f346f1
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-05-15 02:50:00 +0200

    treesitter languages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -948,7 +948,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'css', 'javascript', 'typescript', 'regex', 'zig' } --MARLENE appended css/js/ts/regex/zig
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
