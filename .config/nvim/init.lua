b7bb0b48bf0f0ff0cf618d990196db7251a49c5f
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-05-15 02:41:22 +0200

    which-key documentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -405,9 +405,10 @@ do
     icons = { mappings = vim.g.have_nerd_font },
     -- Document existing key chains
     spec = {
+      { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } }, --MARLENE
       { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
       { '<leader>t', group = '[T]oggle' },
-      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+      { '<leader>g', group = '[G]it', mode = { 'n', 'v' } }, --MARLENE
       { 'gr', group = 'LSP Actions', mode = { 'n' } },
     },
   }
