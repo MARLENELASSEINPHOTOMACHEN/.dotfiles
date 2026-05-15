dfe8db361c789972a3d91d956fccf9e12d72737f
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-05-15 02:49:44 +0200

    blink config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -894,7 +894,9 @@ do
       -- <c-k>: Toggle signature help
       --
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
-      preset = 'default',
+      -- preset = 'default', --MARLENE original
+      preset = 'super-tab', --MARLENE
+      ['<C-y>'] = { 'accept', 'fallback' }, --MARLENE
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
