b437c3e1e51d840064ced091c0d21c0fee4f77b2
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-08-30 23:52:11 +0200

    216e06a Update section 2's name

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -178,7 +178,7 @@ do
 end
 
 -- ============================================================
--- SECTION 2: KEYMAPS
+-- SECTION 2: KEYMAPS & AUTOCMDS
 -- basic keymaps, basic autocmds
 -- ============================================================
 do
