42ea5f912dfbead2b9ebfbf1b2b2342c3b0d3afe
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-09-05 13:04:45 +0200

    845db23 Enable gitsigns.nvim recommended keymaps by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -376,7 +376,8 @@ do
 			topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
 			changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
 		},
-		on_attach = function(bufnr) --MARLENE start gitsigns keymaps
+		-- gitsigns.nvim's recommended keymaps:
+		on_attach = function(bufnr)
 			-- Navigation
 			vim.keymap.set("n", "]c", function()
 				if vim.wo.diff then
@@ -394,6 +395,23 @@ do
 				end
 			end, { desc = "Jump to previous git [c]hange", buffer = bufnr })
 
+			--MARLENE start commented, `<leader>h` prefix collides with harpoon (Section 10)
+			-- Visual mode actions
+			-- vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git [s]tage hunk' })
+			-- vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git [r]eset hunk' })
+			-- Normal mode actions
+			-- vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+			-- vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+			-- vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+			-- vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+			-- vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+			-- vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk [i]nline' })
+			-- vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = 'git [b]lame line' })
+			-- vim.keymap.set('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+			-- vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
+			-- vim.keymap.set('n', '<leader>hQ', function() gitsigns.setqflist 'all' end, { desc = 'git hunk [Q]uickfix list (all files in repo)' })
+			-- vim.keymap.set('n', '<leader>hq', gitsigns.setqflist, { desc = 'git hunk [q]uickfix list (all changes in this file)' })
+			--MARLENE end commented
 			-- Toggles
 			vim.keymap.set(
 				"n",
@@ -409,7 +427,7 @@ do
 			)
 			-- Text object
 			vim.keymap.set({ "o", "x" }, "ih", gitsigns.select_hunk, { buffer = bufnr })
-		end, --MARLENE end gitsigns keymaps
+		end,
 	})
 
 	-- Useful plugin to show you pending keybinds.
