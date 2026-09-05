139ca7969ed0ac184ebce4fa25ca89a4c9dff989
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-09-06 01:22:55 +0300

    0a035d6 Add bufnr back to the gitsigns keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -385,7 +385,7 @@ do
 				else
 					gitsigns.nav_hunk("next")
 				end
-			end, { desc = "Jump to next git [c]hange", buffer = bufnr })
+			end, { desc = "Jump to next git [c]hange", buf = bufnr })
 
 			vim.keymap.set("n", "[c", function()
 				if vim.wo.diff then
@@ -393,40 +393,45 @@ do
 				else
 					gitsigns.nav_hunk("prev")
 				end
-			end, { desc = "Jump to previous git [c]hange", buffer = bufnr })
+			end, { desc = "Jump to previous git [c]hange", buf = bufnr })
 
 			--MARLENE start commented, `<leader>h` prefix collides with harpoon (Section 10)
 			-- Visual mode actions
-			-- vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git [s]tage hunk' })
-			-- vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git [r]eset hunk' })
+			-- vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git [s]tage hunk', buf = bufnr })
+			-- vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git [r]eset hunk', buf = bufnr })
 			-- Normal mode actions
-			-- vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-			-- vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-			-- vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-			-- vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-			-- vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-			-- vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk [i]nline' })
-			-- vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = 'git [b]lame line' })
-			-- vim.keymap.set('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-			-- vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
-			-- vim.keymap.set('n', '<leader>hQ', function() gitsigns.setqflist 'all' end, { desc = 'git hunk [Q]uickfix list (all files in repo)' })
-			-- vim.keymap.set('n', '<leader>hq', gitsigns.setqflist, { desc = 'git hunk [q]uickfix list (all changes in this file)' })
+			-- vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk [i]nline', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = 'git [b]lame line', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hQ', function() gitsigns.setqflist 'all' end, { desc = 'git hunk [Q]uickfix list (all files in repo)', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hq', gitsigns.setqflist, { desc = 'git hunk [q]uickfix list (all changes in this file)', buf = bufnr })
 			--MARLENE end commented
 			-- Toggles
 			vim.keymap.set(
 				"n",
 				"<leader>tb",
 				gitsigns.toggle_current_line_blame,
-				{ desc = "[T]oggle git show [b]lame line", buffer = bufnr }
+				{ desc = "[T]oggle git show [b]lame line", buf = bufnr }
 			)
 			vim.keymap.set(
 				"n",
 				"<leader>tw",
 				gitsigns.toggle_word_diff,
-				{ desc = "[T]oggle git intra-line [w]ord diff", buffer = bufnr }
+				{ desc = "[T]oggle git intra-line [w]ord diff", buf = bufnr }
 			)
 			-- Text object
-			vim.keymap.set({ "o", "x" }, "ih", gitsigns.select_hunk, { buffer = bufnr })
+			vim.keymap.set(
+				{ "o", "x" },
+				"ih",
+				gitsigns.select_hunk,
+				{ desc = "text object [i]nside [h]uhk", buf = bufnr }
+			)
 		end,
 	})
 
