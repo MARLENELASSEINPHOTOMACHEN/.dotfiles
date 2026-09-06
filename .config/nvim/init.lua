e928bf1df9b95d1f55a5af68d08b0d0628586ce3
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-09-06 09:35:36 +0300

    f7b845d Align keymaps with gitsigns.nvim's defaults

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -408,7 +408,7 @@ do
 			-- vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk [i]nline', buf = bufnr })
 			-- vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = 'git [b]lame line', buf = bufnr })
 			-- vim.keymap.set('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index', buf = bufnr })
-			-- vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit', buf = bufnr })
+			-- vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis '~' end, { desc = 'git [D]iff against last commit', buf = bufnr })
 			-- vim.keymap.set('n', '<leader>hQ', function() gitsigns.setqflist 'all' end, { desc = 'git hunk [Q]uickfix list (all files in repo)', buf = bufnr })
 			-- vim.keymap.set('n', '<leader>hq', gitsigns.setqflist, { desc = 'git hunk [q]uickfix list (all changes in this file)', buf = bufnr })
 			--MARLENE end commented
