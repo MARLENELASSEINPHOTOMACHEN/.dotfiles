e19d663d6b7977fd9abf3514d8ff8c91a966be9d
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-09-08 14:15:08 -0400

    3711862 Install the language first

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1092,15 +1092,16 @@ do
 	---@param buf integer
 	---@param language string
 	local function treesitter_try_attach(buf, language)
+		-- Check if a parser exists and load it
+		if not vim.treesitter.language.add(language) then
+			return
+		end
+
 		-- Check if the buffer is valid (might not be after install completes)
 		if not vim.api.nvim_buf_is_valid(buf) then
 			return
 		end
 
-		-- Check if a parser exists and load it
-		if not vim.treesitter.language.add(language) then
-			return
-		end
 		-- Enable syntax highlighting and other treesitter features
 		vim.treesitter.start(buf, language)
 
