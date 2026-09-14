ceb9a94fe8abf7a13074df6a8b4a81bd7534f07f
Marian Werny <34095634+MARLENELASSEINPHOTOMACHEN@users.noreply.github.com>
2026-09-14 17:52:52 +0300

    80743df Small formatting fixes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -333,8 +333,8 @@ do
 	})
 end
 
----Because most plugins are hosted on GitHub, you can use the helper
----function to have less repetition in the following sections.
+--- Because most plugins are hosted on GitHub, you can use the helper
+--- function to have less repetition in the following sections.
 ---@param repo string
 ---@return string
 local function gh(repo)
@@ -600,7 +600,7 @@ do
 	-- Telescope picker. This is really useful to discover what Telescope can
 	-- do as well as how to actually do it!
 
-	---@type (string|vim.pack.Spec)[]
+	---@type (string | vim.pack.Spec)[]
 	local telescope_plugins = {
 		gh("nvim-lua/plenary.nvim"),
 		gh("nvim-telescope/telescope.nvim"),
